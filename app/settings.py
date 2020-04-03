d4b5fa87025a19bc7d85e4ea2ecb2b96b0ce41d5
Lucke <you@example.comrodrigo.lucke@outlook.com>
2020-04-03 20:06:18 -0300

    alterações

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'baseApp'
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -55,14 +55,19 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        #'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+
             ],
         },
     },
--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Escola(models.Model):
-    nome = models.TextField()
-    telefone = models.TextField()
-    email = models.TextField();
+    nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
     criado_por = models.ForeignKey(User, on_delete=models.PROTECT)
     criado_em = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return self.nome
