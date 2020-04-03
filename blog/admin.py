bf3d4048fe4eae4b3b3f1936f6f215368d231fe7
Lucke <you@example.comrodrigo.lucke@outlook.com>
2020-04-03 20:44:52 -0300

    criadomodel aluno

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from blog.models import Escola
+from blog.models import Aluno
+
 admin.site.register(Escola)
+admin.site.register(Aluno)
+
 
--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,13 @@ class Escola(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    criado_por = models.ForeignKey(User, on_delete=models.PROTECT)
+    criado_em = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
